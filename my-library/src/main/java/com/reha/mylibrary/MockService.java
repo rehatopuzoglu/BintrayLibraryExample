20c08e8148e9ab6d69f1016cf1cd66c2ab452a96
rehatopuzoglu <reha.topuzoglu@yolo.com.tr>
2017-11-15 23:46:16 +0300

    first commit

---
+++ my-library/src/main/java/com/reha/mylibrary/MockService.java
@@ -0,0 +1,16 @@
+package com.reha.mylibrary;
+
+/**
+ * Created by reha on 15.11.2017.
+ */
+
+public class MockService {
+
+    /**
+     * useful method for service.
+     * @return name of the best developer ever!
+     */
+    public static String getName() {
+        return "Reha Topuzoğlu";
+    }
+}
